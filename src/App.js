a2ca23188358ae173543125b64fb2a0243d0dacd
김세민 <sermain6240@gmail.com>
2026-02-05 04:21:42 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
-// App.js
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './component/Movie';
+import './App.css';
 
-function Food({name, picture, rating}){
+class App extends React.Component{
+    state = {
+        isLoding: true,
+        movies: [],
+    };
 
-    return (
-        <div>
-            <h2>I like {name} </h2>
-            <img src = {picture} alt = {name}/>
-            <h4> {rating}/5.0 </h4>
-        </div>
-    
-    )
+    getMovies = async () => {
+        const {
+           data: {
+                data: {movies},
+            },
+        }  = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        this.setState({movies, isLoding: false});
+    }
+
+
+    componentDidMount(){
+        //영화 데이터 로딩!
+        this.getMovies();
+ 
+    }
+
+    render() {
+        const {isLoding, movies} = this.state;
+        return (
+              <section className = "container">
+                     {isLoding ? (
+                        <div className = "lodder">
+                            <span className = "loader__text">Loading...</span>\
+                        </div>
+                     ) : (
+                        <div className = "movie">
+                           {movies.map(movie => (
+                            <Movie
+                                key = {movie.id}
+                                id = {movie.id}
+                                year = {movie.year}
+                                title = {movie.title}
+                                summary = {movie.summary}
+                                poster = {movie.medium_cover_image}
+                                genres = {movie.genres}
+                            />
+                           ))}
+                        </div>
+                     )}
+                </section>
+        )
+    }
 }
 
-const foodIlike = [
-    
-    {
-        id: 1,
-        name: 'Kimchi',
-        image: "https://i.namu.wiki/i/O1Z-Nm3Bq-grP5ebmnPeizxI6nmij93lv8GOc3PE_c3R-GLSiwljFk7N-7FBD0_hKyB5esEjjMXxRWzj0ypZ3w.webp",
-        rating: 5,
-    },
-
-    {
-        id: 2,
-        name: 'Samgyeopsal',
-        image:'https://i.namu.wiki/i/oFHlYDjoEh8f-cc3lNK9jAemRkbXxNGwUg7XiW5LGS6DF1P2x8GCeNQxbQhVIwtUS1u53YPw-uoyqpmLtrGNJA.webp',
-        rating: 4,
-    },
-
-     {
-        id: 3,
-        name: 'Bibimbap',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/250px-Dolsot-bibimbap.jpg',
-        rating: 3,
-    },
-
-];
-
-
-function App() {
-    return (
-        <div>
-          {foodIlike.map(dish => (<Food key = {dish.id} name = {dish.name} picture = {dish.image} rating={dish.rating}/>))}
-        </div>
-        
-    );
-}
-
-Food.PropTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating:PropTypes.number.isRequired,
-};
 
 export default App;
